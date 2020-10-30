02515b896290f42b8fb0b7ef844c06d4c1d95a98
yuweixiong <542708753@qq.com>
2020-10-30 18:01:34 +0800

    更新

---
+++ CommonUtil/src/main/java/other/HumpUtil.java
@@ -0,0 +1,54 @@
+package other;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author yuweixiong
+ * @date 2020/10/15 15:46
+ * @description
+ */
+public class HumpUtil {
+
+    private static Pattern linePattern = Pattern.compile("_(\\w)");
+    private static Pattern humpPattern = Pattern.compile("[A-Z]");
+
+    /**
+     * 下划线转驼峰
+     *
+     * @param str
+     * @return
+     */
+    public static String lineToHump(String str) {
+        str = str.toLowerCase();
+        Matcher matcher = linePattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+
+    /**
+     * 驼峰转下划线
+     *
+     * @param str
+     * @return
+     */
+    public static String humpToLine(String str) {
+        Matcher matcher = humpPattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, "_" + matcher.group(0).toLowerCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String lineToHump = lineToHump("f_parent_no_leader");
+        System.out.println(lineToHump);// fParentNoLeader
+        System.out.println(humpToLine(lineToHump));// f_parent_no_leader
+    }
+}
